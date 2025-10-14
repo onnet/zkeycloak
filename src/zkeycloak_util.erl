506009541e8a28ca7ba24767eb13f0d16d0500da
root <root@kz530.onnet.su>
2025-10-14 07:08:27 +0000

    upd

--- src/zkeycloak_util.erl
+++ src/zkeycloak_util.erl
@@ -5,6 +5,8 @@
         ,client_id_atom/0
         ,client_id/0
         ,client_secret/0
+        ,redirect_uri/0
+        ,preferred_auth_methods/0
         ,retrieve_token/1
         ]
        ).
@@ -25,6 +27,18 @@ client_id() ->
 client_secret() ->
     kapps_config:get_ne_binary(<<"zkeycloak">>, <<"client_secret">>, <<"client_secret">>).
 
+-spec redirect_uri() -> kz_term:ne_binary().
+redirect_uri() ->
+    kapps_config:get_ne_binary(<<"zkeycloak">>, <<"redirect_uri">>, <<"redirect_uri">>).
+
+-spec preferred_auth_methods() -> kz_term:ne_binary().
+preferred_auth_methods() ->
+    case kapps_config:get(<<"zkeycloak">>, <<"preferred_auth_methods">>, [client_secret_basic, client_secret_post]) of
+        L when is_list(L) ->
+            [kz_term:to_atom(V, 'true') || V <- L];
+        _ -> []
+    end.
+
 -spec auth_url() -> kz_term:ne_binary().
 auth_url() ->
     {ok, RedirectUri} =
@@ -32,8 +46,8 @@ auth_url() ->
             client_id_atom()
             ,client_id()
             ,client_secret()
-            ,#{redirect_uri => <<"http://192.168.111.205:8000/ext/login_key_callback">>
-              ,preferred_auth_methods => [client_secret_basic, client_secret_post]
+            ,#{'redirect_uri' => redirect_uri()
+              ,'preferred_auth_methods' => preferred_auth_methods() 
               }
         ),
     kz_binary:join(RedirectUri, <<"">>).
@@ -46,8 +60,8 @@ retrieve_token(AuthCode) ->
             ,client_id_atom()
             ,client_id()
             ,client_secret()
-            ,#{redirect_uri => <<"http://192.168.111.205:8000/ext/login_key_callback">>
-              ,'preferred_auth_methods' => ['client_secret_basic', 'client_secret_post']
+            ,#{'redirect_uri' => redirect_uri()
+              ,'preferred_auth_methods' => preferred_auth_methods()
               }
         ),
     Token.
