105134d6056b81fbf82e9c76bf3aec786875833d
root <root@kz530.onnet.su>
2025-10-14 19:35:21 +0000

    upd

--- src/crossbar/cb_zkeycloak_ext.erl
+++ src/crossbar/cb_zkeycloak_ext.erl
@@ -18,6 +18,7 @@
 -define(HEADERS, [{"content-type", "application/json"}]).
 
 -define(AUTH_LINK, <<"auth_link">>).
+-define(AUTH_CALLBACK, <<"auth_callback">>).
 -define(ZKEYCLOAK, <<"zkeycloak_ext">>).
 
 -spec init() -> ok.
@@ -34,12 +35,14 @@ init() ->
 allowed_methods() -> [?HTTP_POST, ?HTTP_GET].
 -spec allowed_methods(path_token()) -> http_methods().
 allowed_methods(?AUTH_LINK) -> [?HTTP_GET];
+allowed_methods(?AUTH_CALLBACK) -> [?HTTP_GET];
 allowed_methods(?ZKEYCLOAK) -> [?HTTP_POST, ?HTTP_GET].
 
 -spec resource_exists() -> boolean().
 resource_exists() -> 'true'.
 -spec resource_exists(path_tokens()) -> boolean().
 resource_exists(?AUTH_LINK) -> 'true';
+resource_exists(?AUTH_CALLBACK) -> 'true';
 resource_exists(?ZKEYCLOAK) -> 'true'.
 
 -spec authorize(cb_context:context()) -> boolean() | {'stop', cb_context:context()}.
@@ -69,6 +72,9 @@ authorize_nouns(_Context, [{<<"zkeycloak_ext">>, []}], Method) when Method =:= ?
 authorize_nouns(_Context, [{<<"zkeycloak_ext">>, [<<"auth_link">>]}], Method) when Method =:= ?HTTP_GET ->
     lager:info("authorize_nouns_zkeycloak_ext authorizing zkeycloak_ext"),
     'true';
+authorize_nouns(_Context, [{<<"zkeycloak_ext">>, [<<"auth_callback">>]}], Method) when Method =:= ?HTTP_GET ->
+    lager:info("authorize_nouns_zkeycloak_ext authorizing zkeycloak_ext"),
+    'true';
 authorize_nouns(_, _Nouns, _) ->
     lager:info("authorize_nouns_zkeycloak_ext undefined _Nouns: ~p", [_Nouns]),
     'false'.
@@ -87,23 +93,14 @@ authenticate(Context, Token1) ->
 
 authenticate_nouns(Context, [{<<"zkeycloak_ext">>, []}]) ->
     lager:info("authenticate_nouns/2  req_headers: ~p",[cb_context:req_headers(Context)]),
-%%    WebHookSecretToken = kapps_config:get_ne_binary(<<"zkeycloak">>,
-%%                                                    <<"tlg_secret_token">>,
-%%                                                    <<"pls_install_value_in_db">>),
-%%    case maps:get(<<"x-telegram-bot-api-secret-token">>, cb_context:req_headers(Context), <<>>) of
-%%        WebHookSecretToken ->
-%%            lager:info("authenticate_nouns/1: true"),
-%%            true;
-%%        _ -> lager:info("authenticate_nouns/1: false"),
-%%             false
-%%    end;
     'true';
 authenticate_nouns(_Context, [{<<"zkeycloak_ext">>, [<<"auth_link">>]}]) ->
+    'true';
+authenticate_nouns(_Context, [{<<"zkeycloak_ext">>, [<<"auth_callback">>]}]) ->
     'true';
 authenticate_nouns(_Context, _Nouns) ->
     lager:info("authenticate_nouns/1 _Nouns: ~p",[_Nouns]),
     'false'.
-%%'true'.
 
 -spec validate(cb_context:context()) -> cb_context:context().
 validate(Context) ->
@@ -116,7 +113,6 @@ validate(Context) ->
 
 -spec validate(cb_context:context(), path_token()) -> cb_context:context().
 validate(Context, ?AUTH_LINK) ->
-    lager:info("validate_ext/2  req_files: ~p",[cb_context:req_files(Context)]),
     lager:info("validate_ext/2  req_headers: ~p",[cb_context:req_headers(Context)]),
     lager:info("validate_ext/2  req_nouns: ~p",[cb_context:req_nouns(Context)]),
     lager:info("validate_ext/2  req_verb: ~p",[cb_context:req_verb(Context)]),
@@ -124,6 +120,64 @@ validate(Context, ?AUTH_LINK) ->
     AuthUrl = zkeycloak_util:auth_url(),
     JObj = kz_json:set_value(<<"auth_url">>, AuthUrl, kz_json:new()),
     cb_context:set_resp_status(cb_context:set_resp_data(Context, JObj), 'success');
+validate(Context, ?AUTH_CALLBACK) ->
+    lager:info("validate_ext/2  req_files: ~p",[cb_context:req_files(Context)]),
+    lager:info("validate_ext/2  req_headers: ~p",[cb_context:req_headers(Context)]),
+    lager:info("validate_ext/2  req_nouns: ~p",[cb_context:req_nouns(Context)]),
+    lager:info("validate_ext/2  req_verb: ~p",[cb_context:req_verb(Context)]),
+    lager:info("validate_ext/2  req_id: ~p",[cb_context:req_id(Context)]),
+    lager:info("validate_ext/2  cb_context:query_string: ~p",[cb_context:query_string(Context)]),
+    QS = cb_context:query_string(Context),
+    Code = kz_json:get_ne_binary_value(<<"code">>, QS),
+    case zkeycloak_util:retrieve_token(Code) of
+        {oidcc_token
+            ,{oidcc_token_id, TokenId, ClaimsMap}
+            ,{oidcc_token_access, TokenAccess, _Timeout, _Type}
+            ,{oidcc_token_refresh, TokenRefresh}
+            ,_Scope
+        } ->
+            lager:info("validate_ext/2  TokenId: ~p",[TokenId]),
+            lager:info("validate_ext/2  TokenAccess: ~p",[TokenAccess]),
+            lager:info("validate_ext/2  TokenRefresh: ~p",[TokenRefresh]),
+            ClaimsProps = kz_json:to_proplist(kz_json:from_map(ClaimsMap)),
+            %%OwnerId = props:get_value(<<"owner_id">>, ClaimsProps),
+            AccountId = props:get_value(<<"account_id">>, ClaimsProps),
+            OwnerId = zbrt_util:from_key(props:get_value(<<"sub">>, ClaimsProps)),
+            DbName = kzs_util:format_account_id(AccountId,'encoded'),
+            case kz_datamgr:open_doc(DbName, OwnerId) of
+                {'ok', _} -> 'ok';
+                _ ->
+                    Firstname = props:get_value(<<"given_name">>, ClaimsProps),
+                    Surname = props:get_value(<<"family_name">>, ClaimsProps),
+                    Email = props:get_value(<<"email">>, ClaimsProps),
+                    Phonenumber = <<"">>,
+                    UserPassword = kz_binary:rand_hex(12),
+                    zkeycloak_util:create_user(AccountId, OwnerId, Firstname, Surname, Email, Phonenumber, UserPassword)
+            end,
+            lager:info("validate_ext/2  ClaimsProps: ~p",[ClaimsProps]),
+            Setters = [{fun cb_context:set_auth_token/2, TokenAccess}
+                      ,{fun cb_context:set_auth_doc/2, kz_json:from_list(ClaimsProps)}
+                      ],
+            Props = props:filter_undefined(
+                      [{<<"account_id">>, AccountId}
+                      ,{<<"owner_id">>, OwnerId}
+                      ]),
+            Resp = crossbar_util:response_auth(kz_json:from_list(Props), AccountId, OwnerId),
+%
+%
+%            lager:debug("created new local auth token: ~s", [kz_json:encode(Resp)]),
+%
+%            log_success_auth(Method, <<"jwt_auth_token">>, <<"authentication resulted in token creation">>, Context, AccountId, AuthConfig),
+%
+%            lager:info("create_auth_token JObj: ~p", [JObj]),
+%            lager:info("create_auth_token RespObj: ~p", [RespObj]),
+%            lager:info("create_auth_token Claims: ~p", [Claims]),
+%            lager:info("create_auth_token AuthConfig: ~p", [AuthConfig]),
+%            lager:info("create_auth_token Method: ~p", [Method]),
+            crossbar_util:response(Resp, cb_context:setters(Context, Setters));
+        _ ->
+            cb_context:add_system_error('invalid_credentials', Context)
+    end;
 validate(Context, ?ZKEYCLOAK) ->
     lager:info("validate_ext/2  req_files: ~p",[cb_context:req_files(Context)]),
     lager:info("validate_ext/2  req_headers: ~p",[cb_context:req_headers(Context)]),
--- src/zkeycloak_util.erl
+++ src/zkeycloak_util.erl
@@ -8,8 +8,20 @@
         ,redirect_uri/0
         ,preferred_auth_methods/0
         ,retrieve_token/1
+        ,create_user/7
         ]
        ).
+
+-define(MK_USER,
+        {[{<<"enabled">>, 'true'}
+         ,{<<"priv_level">>,<<"user">>}
+         ,{<<"vm_to_email_enabled">>,true}
+         ,{<<"fax_to_email_enabled">>,true}
+         ,{<<"verified">>,false}
+         ,{<<"timezone">>,<<"UTC">>}
+         ,{<<"record_call">>,false}
+         ,{<<"pvt_type">>, kzd_users:type()}
+         ]}).
 
 -spec issuer() -> kz_term:ne_binary().
 issuer() ->
@@ -65,3 +77,44 @@ retrieve_token(AuthCode) ->
               }
         ),
     Token.
+
+-spec create_user(kz_term:ne_binary()
+                 ,kz_term:ne_binary()
+                 ,kz_term:ne_binary()
+                 ,kz_term:ne_binary()
+                 ,kz_term:ne_binary()
+                 ,kz_term:ne_binary()
+                 ,kz_term:ne_binary()
+                 ) -> {'ok', kz_json:object()} | kz_datamgr:data_error().
+create_user(AccountId, UserDocId, Firstname, Surname, Email, Phonenumber, UserPassword) ->
+    lager:info("create_user AccountId: ~p, UserDocId: ~p, Email: ~p",[AccountId,UserDocId,Email]),
+    lager:info("create_user Firstname: ~p",[Firstname]),
+    lager:info("create_user Surname: ~p",[Surname]),
+    lager:info("create_user Phonenumber: ~p",[Phonenumber]),
+    Props = props:filter_empty([{<<"username">>, Email}
+                               ,{<<"first_name">>, Firstname}
+                               ,{<<"last_name">>, Surname}
+                               ,{<<"email">>, Email}
+                               ,{<<"contact_phonenumber">>, Phonenumber}
+                               ,{<<"password">>, UserPassword}
+                               ,{<<"priv_level">>, <<"admin">>}
+                               ]),
+    DbName = kzs_util:format_account_db(AccountId),
+    Ctx0 = cb_context:set_account_id(cb_context:new(), AccountId),
+    Ctx1 = cb_context:set_db_name(Ctx0, DbName),
+    UDoc = kz_json:set_values(Props, ?MK_USER),
+    case catch kzd_users:validate(AccountId, UserDocId, UDoc) of
+        {true, UDoc1} ->
+            UDoc2 = crossbar_doc:update_pvt_parameters(UDoc1, Ctx1),
+            UDoc3 = kz_json:set_value(<<"pvt_type">>,<<"user">>,UDoc2),
+            Creates = kz_json:to_proplist(UDoc3),
+            UpdateOptions = [{'create', Creates}
+                            ,{'update', Creates}
+                            ,{'ensure_saved', 'true'}
+                            ],
+            Result = kz_datamgr:update_doc(DbName, UserDocId, UpdateOptions),
+            lager:info("create_user Result: ~p", [Result]);
+        Err ->
+            lager:info("create_user Err: ~p", [Err])
+    end.
+
